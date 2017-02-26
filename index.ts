90695fc7880f0878cb0953ba2c54c2c86a80f0b5
Michael Sholty <michael.sholty@sharpspring.com>
2017-02-26 16:24:14 -0500

    feat: add rooms route

--- index.ts
+++ index.ts
@@ -1,22 +1,46 @@
 import * as path from 'path';
 import * as express from 'express';
-import * as serveIndex from 'serve-index';
-import { createServer } from 'http';
+import { readFileSync } from 'fs';
+import { createServer } from 'https';
 import { Server } from 'colyseus';
 import { DraftRoom } from "./app/DraftRoom";
+import * as cors from 'cors';
+import * as bodyParser from 'body-parser';
 
 const port = process.env.PORT || 2657;
 const app = express();
-const httpServer = createServer(app);
-const gameServer = new Server({ server: httpServer });
+const httpsServer = createServer({
+  key: readFileSync('key.pem'),
+  cert: readFileSync('cert.pem')
+}, app);
+const gameServer = new Server({ server: httpsServer });
 
-gameServer.register("draft", DraftRoom, {
+app.use(cors());
+app.use(bodyParser.json());
+
+gameServer.register('draft', DraftRoom, {
   maxClients: 10
 });
 
-app.use(express.static(path.join(__dirname, "static")));
-app.use('/', serveIndex(path.join(__dirname, "static"), { icons: true }))
+app.route('/rooms')
+  .get((req, res) => {
+    const rooms = gameServer.getRoomsById();
+    const availableRooms = gameServer.getAvailableRooms();
+    console.log(rooms);
+    res.send({
+      rooms,
+      availableRooms,
+    });
+  }).put((req, res) => {
+    const id = req.body.id;
+    console.log(req.body);
+    console.log(`creating draft/${id}`);
+    gameServer.register(`draft/${id}`, DraftRoom, {
+      maxClients: 10,
+    });
+    res.send(id);
+  });
 
-httpServer.listen(port);
+httpsServer.listen(port);
 
-console.log(`Listening on http://localhost:${ port }`);
+console.log(`Listening on https://localhost:${ port }`);
